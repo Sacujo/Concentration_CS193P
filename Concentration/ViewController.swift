cec745c430b0f4effffac6aab1143431ae4f814f
Igor Guryan <sacujo@mail.ru>
2020-03-10 11:33:12 +0300

    Initial Commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Igor Guryan on 10.03.2020.
+//  Copyright © 2020 Igor Guryan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
